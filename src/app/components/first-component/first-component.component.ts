659e8650b3fde492b67f809853aa1331a273b114
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-18 09:06:40 +0100

    Interpolar Dados(mostrar em tela)

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Guilherme Martins';
+  age: number = 29;
+  job: string = 'Software Engineer - Frontend';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
